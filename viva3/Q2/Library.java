082e0a24efd7bda55e9a0c6b857adc17ad283e05
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2023-12-24 14:01:39 +0800

    Update Library.java

--- viva3/Q2/Library.java
+++ viva3/Q2/Library.java
@@ -5,35 +5,26 @@ import java.util.ArrayList;
 public class Library {
     private ArrayList<Book> books = new ArrayList<>();
 
-    public Book search(String title) {
-        // binary search on titles
-        int low = 0;
-        int high = books.size() - 1;
-        
-        while (low <= high) {
-            int mid = (low + high) / 2;
-
-            if (title.compareTo(books.get(mid).getTitle()) < 0) {
-                high = mid;
-            }
-            else if (title.compareTo(books.get(mid).getTitle()) > 0) {
-                low = mid + 1;
-            }
-            else {
-                return books.get(mid);
+    public Book findBooksByTitle(String title) {
+        for (int i = 0; i < this.books.size(); i++) {
+            Book book = this.books.get(i);
+            if (book.getTitle().equals(title)) {
+                book.display();
+                return book;
             }
         }
-        System.out.println("Did not find a matching book.");
+        
         return null;
     }
     
-    public ArrayList<Book> findAll(String author) {
+    public ArrayList<Book> findBooksByAuthor(String author) {
         // Return a list of all books from the author, or null
         ArrayList<Book> result = new ArrayList<>();
         
         for (int i = 0; i < this.books.size(); i++) {
             Book book = this.books.get(i);
             if (book.getAuthor().equals(author)) {
+                book.display();
                 result.add(book);
             }
         }
@@ -41,58 +32,59 @@ public class Library {
         return (result.isEmpty()) ? null : result;
     }
     
-    public boolean borrow(Book book) {
+    public Book findBookByISBN(String ISBN){
+        for (int i = 0; i < this.books.size(); i++) {
+            Book book = this.books.get(i);
+            if (book.getISBN().equals(ISBN)) {
+                return book;
+            }
+        }
+        
+        return null;
+    }
+    
+    public boolean borrowBook(String ISBN) {
+        int d1 = ISBN.charAt(ISBN.length()-1) - '0';
+        int d2 = ISBN.charAt(ISBN.length()-2) - '0';
+        
+        if (isPrime(Integer.valueOf(d1)) || isPrime(Integer.valueOf(d2))) {
+            return false;
+        }
+        
+        Book book = this.findBookByISBN(ISBN);
         if (book == null) {
-            System.out.println("Invalid request.");
             return false;
         }
         
-        Book b = this.search(book.getTitle());
-        if (b == null) {
-            System.out.println("This book does not exist in the library");
-            return false;
-        }
-        
-        String ISBN = b.getISBN();
-        int endCode = Integer.parseInt(ISBN) % 100;
-        
-        if (isPrime(endCode)) {
-            System.out.println("This book cannot be borrowed");
-            return false;
-        }
-        
-        b.setBorrowed(true);
-        System.out.println("Borrowed book with ISBN: " + b.getISBN());
+        book.setBorrowed(true);
         return true;
     }
     
     public void returnBook(Book book) {
-        Book b = this.search(book.getTitle());
+        Book b = this.findBooksByTitle(book.getTitle());
         if (b == null) {
-            System.out.println("This book does not belong in this library.");
             return;
         }
         
         b.setBorrowed(false);
     }
     
-    public void add(Book book) {
+    public void addBook(Book book) {
         this.books.add(book);
-        sortBooks(); // sort after adding a book so binary search can be done
     }
     
-    public boolean remove(Book book) {
-        if (this.books.remove(book)) {
-            System.out.println("Succesfully removed book with ISBN: " + book.getISBN());
-            return true;
+    public boolean removeBook(String ISBN) {
+        Book book = findBookByISBN(ISBN);
+        if (book == null) {
+            return false;
         }
-        
-        System.out.println("Did not find the specified book");
-        return false;
+
+        return this.books.remove(book);
     }
     
     public void sortBooks() {
         mergeSortBooks(this.books, 0, this.books.size() - 1);
+        this.displayLibrary();
     }
     
     private boolean isPrime(int n) {
@@ -158,4 +150,12 @@ public class Library {
             books.set(start + i, temp[i]);
         }
     }    
+    
+    public void displayLibrary() {
+        System.out.println("Books in the library, sorted by title:");
+        for (int i = 0; i < this.books.size(); i++) {
+            this.books.get(i).display();
+            System.out.println();
+        }
+    }
 }
