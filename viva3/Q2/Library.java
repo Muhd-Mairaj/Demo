4d5f884e61bbb7c2df15318ecbfc04a422a27f8d
Muhd-Mairaj <93455017+Muhd-Mairaj@users.noreply.github.com>
2023-12-27 21:51:38 +0800

    Fixed bug in isPrime method

--- viva3/Q2/Library.java
+++ viva3/Q2/Library.java
@@ -74,16 +74,19 @@ public class Library {
     }
     
     private boolean isPrime(int n) {
-        boolean prime = n > 1;
+        if (n == 2) {
+            return true;
+        }
         
-        for (int i = 3; i < n; i++) {
+        boolean prime = n > 1;
+        for (int i = 2; i < n; i++) {
             if (n % i == 0) {
                 prime = false;
                 break;
             }
         }
         
-        return prime;
+        return prime ;
     }
         
     public void displayLibrary() {
